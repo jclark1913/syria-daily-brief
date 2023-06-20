3d43ff64788e24c8cc23f8abb3c1d8f02f60696f
Justin Clark <jclarksummit@gmail.com>
2023-06-20 12:35:20 -0400

    Add scaffolding for sdf-press scraper

---
+++ scrapers/sdfpress.py
@@ -0,0 +1,21 @@
+import requests
+
+import time
+import datetime
+
+from bs4 import BeautifulSoup
+from globalscrape import DEFAULT_HEADERS, ARABIC_LATIN_MONTHS
+
+def get_sdfpress_data(date):
+    """"""
+
+def get_news_articles_by_page(page_num=1, stop_timestamp=False):
+    """"""
+
+def get_article_text(article_link):
+    """"""
+
+def get_timestamp(date):
+    """Converts Arabic date in 'YYYY, dd month' format to unix timestamp"""
+
+
