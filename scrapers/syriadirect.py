4d18e008fa9f788bbd6d39aed8877254aa223369
Justin Clark <jclarksummit@gmail.com>
2023-06-26 18:03:11 -0400

    Add func to get text content from given article

--- scrapers/syriadirect.py
+++ scrapers/syriadirect.py
@@ -16,6 +16,23 @@ def get_article_text(article_link):
     """Concatenates all paragraph elements in article into a single string,
     returns it"""
 
+    # bs4 setup
+    response = requests.get(article_link, headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Identifies paragraphs and creates empty variable for text content
+    paragraphs = soup.find("div", class_="sd_article_body").find_all("p", recursive=False)
+    text_content = ""
+
+    # Iterates thru paragraph elements and concatenates all elements containing text
+    for paragraph in paragraphs:
+        elements = paragraph.find_all(True)
+        for element in elements:
+            text_content = text_content + element.text
+        text_content = text_content + "\n\n"
+
+    return text_content
+
 def get_timestamp(date):
     """Takes date input and converts it to Unix timestamp
 
