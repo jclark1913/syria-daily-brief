a19e843f88a811562434dc3b310059d4e6c94cf8
Justin Clark <jclarksummit@gmail.com>
2023-07-03 14:40:54 -0400

    Add function to generate # of seconds from Arabic article description

--- scrapers/deirezzor24.py
+++ scrapers/deirezzor24.py
@@ -4,7 +4,7 @@ import time
 import datetime
 
 from bs4 import BeautifulSoup
-from globalscrape import DEFAULT_HEADERS
+from globalscrape import DEFAULT_HEADERS, ARABIC_TIME_UNITS
 
 
 def get_deirezzor24_data(date):
@@ -28,5 +28,49 @@ def get_approx_timestamp_from_desc(desc):
     Assuming current time is 6/20/23 9pm (1687309200):
 
     "5 ساعات ago" -> (1687291200)
+    """
+
+
+
+def get_total_seconds_from_desc(desc):
+    """Returns the total number of seconds from the posted before section of an
+    article. Useful for generating a unix timestamp by subtracting result from
+    current time.
+
+    examples:
+
+    "5 ساعات ago" -> 18000 (3600 * 5)
+    "8 سنوات ago" -> 252455408 (63113852 * 8)
+    "دقيقتين ago" -> 120 (60 * 2)
 
     """
+
+    # Return variable
+    total_seconds = 0
+
+    # Create array of Arabic unit + quantity (if present)
+    arabic_posted = [word for word in desc.split() if word != "ago"]
+
+    # The word for "one" in Arabic (minus gender ending)
+    arabic_one = "واحد"
+
+    # Ascertains duration (Arabic) and number of units
+    if len(arabic_posted) == 1:
+        duration = arabic_posted[0]
+        quantity = 2
+    elif arabic_one in arabic_posted[1]:
+        duration = arabic_posted[0]
+        quantity = 1
+    else:
+        duration = arabic_posted[1]
+        quantity = int(arabic_posted[0])
+
+    print("Duration = ", duration)
+    print("Quantity = ", quantity)
+
+    # Generate # of seconds from Arabic time units dictionary
+    for unit in ARABIC_TIME_UNITS:
+        if duration in ARABIC_TIME_UNITS[unit]["arabic"]:
+            total_seconds = ARABIC_TIME_UNITS[unit]["value"] * quantity
+
+    return total_seconds
