e339e7bfe72ea58ce71b831515a2040443f5e32c
Justin Clark <jclarksummit@gmail.com>
2023-07-05 14:13:06 -0400

    Add function to get text from article link

--- scrapers/houranfreeleague.py
+++ scrapers/houranfreeleague.py
@@ -15,8 +15,17 @@ def get_news_articles_by_page(page_num=1, stop_timestamp=False):
 def get_article_text(article_link):
     """"""
 
+    #bs4 setup
+    response = requests.get(article_link, headers=DEFAULT_HEADERS)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # iterates thru paragraphs and concatenates text content
+    paragraphs = soup.find(
+        "div", class_="entry-content entry clearfix").find_all("p", recursive=False)
+    return "\n\n".join(paragraph.text for paragraph in paragraphs)
+
 def get_timestamp(date):
-    """Converts Arabic date in 'YYYY, dd month' format to unix timestamp"""
+    """Converts Arabic date in 'dd, m, YYYY' format to unix timestamp"""
 
     # Get number of Arabic month and replace it in string
     for month in ARABIC_LATIN_MONTHS:
