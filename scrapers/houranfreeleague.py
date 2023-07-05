92a9f4aa7b57650f6ecdc64a03983234d93d3797
Justin Clark <jclarksummit@gmail.com>
2023-07-05 14:05:31 -0400

    Add function to get timestamp from Arabic date

---
+++ scrapers/houranfreeleague.py
@@ -0,0 +1,28 @@
+import requests
+
+import time
+import datetime
+
+from bs4 import BeautifulSoup
+from globalscrape import DEFAULT_HEADERS, ARABIC_LATIN_MONTHS
+
+def get_hfl_data(date):
+    """"""
+
+def get_news_articles_by_page(page_num=1, stop_timestamp=False):
+    """"""
+
+def get_article_text(article_link):
+    """"""
+
+def get_timestamp(date):
+    """Converts Arabic date in 'YYYY, dd month' format to unix timestamp"""
+
+    # Get number of Arabic month and replace it in string
+    for month in ARABIC_LATIN_MONTHS:
+        if month in date:
+            translated_date = date.replace(month, ARABIC_LATIN_MONTHS[month])
+
+    # Get unix timestamp from translated date
+    return time.mktime(datetime.datetime.strptime(translated_date, "%d %m، %Y").timetuple())
+
