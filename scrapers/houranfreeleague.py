00527f9448a63179582c710597256bc9caab8a8c
Justin Clark <jclarksummit@gmail.com>
2023-07-05 14:37:11 -0400

    Add function to scrape HFL articles until given timestamp

--- scrapers/houranfreeleague.py
+++ scrapers/houranfreeleague.py
@@ -10,10 +10,59 @@ def get_hfl_data(date):
     """"""
 
 def get_news_articles_by_page(page_num=1, stop_timestamp=False):
-    """"""
+    """Scrapes a single page of HFL articles until time limit reached.
+    TODO: Handle server timeout while looping
+    """
+
+    #bs4 setup
+    response = requests.get(
+        f"https://www.horanfree.com/page/{page_num}?cat=%2A"
+    )
+    soup = BeautifulSoup(response.content, "html.parser")
+    articles = soup.find_all("li", class_="post-item")
+
+    # List of articles to be returned
+    article_list = []
+
+    count = 1
+
+    # Gathers article info for each post on single page
+    for a in articles:
+        # identifies date posted and generates Unix timestamp
+        date_posted = a.find("span", class_="date meta-item tie-icon").text
+        current_timestamp = get_timestamp(date_posted)
+        link = a.find("a", class_=None).get("href")
+
+        # Verifies that current_timestamp is less than (earlier than) limit,
+        # breaks loop if so.
+        if stop_timestamp and current_timestamp < stop_timestamp:
+            break
+
+        # Gets title and basic data and creates dict for article
+        article = {
+            "title": a.find("h2", class_="post-title").text,
+            "date_posted": current_timestamp,
+            "link": link,
+            "full_text": get_article_text(link)
+        }
+
+        print(count)
+        count += 1
+        article_list.append(article)
+
+    # Recursively call function for next page until stop_timestamp is reached
+    if stop_timestamp and current_timestamp >= stop_timestamp:
+        next_page_num = page_num + 1
+        article_list += get_news_articles_by_page(
+            page_num=next_page_num, stop_timestamp=stop_timestamp
+        )
+
+    return article_list
+
 
 def get_article_text(article_link):
-    """"""
+    """Gathers article text content from link to given article. Concatenates all
+    paragraph elements into single string and returns it."""
 
     #bs4 setup
     response = requests.get(article_link, headers=DEFAULT_HEADERS)
