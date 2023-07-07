c95ec9571f05d72ea0281ace81c99493c6759b5d
Justin Clark <jclarksummit@gmail.com>
2023-07-07 16:02:10 -0400

    Add Flask boilerplate

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+from flask import Flask
+
+from models import connect_db
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+connect_db(app)
