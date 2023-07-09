4d3102c5de55a81a81940475ada3f86c97f137c0
Justin Clark <jclarksummit@gmail.com>
2023-07-09 14:00:44 -0400

    Add models for Collection and Entry

--- models.py
+++ models.py
@@ -11,3 +11,70 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Collection(db.Model):
+    """The top-level table. A collection points to a specific grouping of
+    scraped data."""
+
+    __tablename__ = 'collections'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    description = db.Column(
+        db.Text,
+        nullable=True,
+    )
+
+    created_at = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+class Entry(db.Model):
+    """A single data point scraped from a website. Linked to a collection."""
+
+    __tablename__ = 'entries'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    title = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    publication = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    full_text = db.Column(
+        db.Text,
+    )
+
+    link = db.Column(
+        db.Text,
+    )
+
+    date_posted = db.Column(
+        db.Text,
+    )
+
+    ai_summary = db.Column(
+        db.Text,
+    )
+
+    collection_id = db.Column(
+        db.Integer,
+        db.ForeignKey('collections.id', ondelete='CASCADE'),
+    )
+
+
+
