2969d46020d65997d8cb39b5bb73b8c32b839728
Justin Clark <jclarksummit@gmail.com>
2023-07-10 14:48:07 -0400

    Update seed.py to generate test collection when seeding data

--- seed.py
+++ seed.py
@@ -4,4 +4,8 @@ from app import db
 db.drop_all()
 db.create_all()
 
+# Create a test collection
+test_collection = Collection(id=1, description="TEST")
+
+db.session.add(test_collection)
 db.session.commit()
