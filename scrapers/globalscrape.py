a36cc44fed4e06016bf6dc53b7dac44543965011
Justin Clark <jclarksummit@gmail.com>
2023-07-10 10:29:17 -0400

    Adjusted formatting (whitespace)

--- scrapers/globalscrape.py
+++ scrapers/globalscrape.py
@@ -1,6 +1,8 @@
 import time
 import datetime
 
+# Global variables to assist with scraping
+
 DEFAULT_HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
 }
