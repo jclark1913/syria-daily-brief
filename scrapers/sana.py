1021f8491bc076312512fe988ac3b4e7744df330
Justin Clark <jclarksummit@gmail.com>
2023-07-11 14:54:52 -0400

    Fix spelling error and change class name to SANA

--- scrapers/sana.py
+++ scrapers/sana.py
@@ -4,7 +4,7 @@ import datetime
 from base_scraper import Base_Scraper
 
 
-class SANA_Scraper(Base_Scraper):
+class SANA(Base_Scraper):
     def __init__(self):
         self.url_template = "https://sana.sy/?cat=29582&paged={page_num}"
         self.publication = "SANA (Syrian Arab News Network)"
@@ -12,9 +12,7 @@ class SANA_Scraper(Base_Scraper):
     # def get_data(self, stop_timestamp):
 
     def get_news_articles_by_page(self, page_num=1, stop_timestamp=False):
-        """Scrapes a single page of sana articles until time limit reached
-        TODO: Handle server timeout while looping
-        """
+        """Scrapes a single page of SANA articles until time limit reached"""
 
         # Generate correct url from template
         url = self.url_template.format(page_num=page_num)
