580ba7f803c5ccf98ef23a8c339ee50f46599a29
Justin Clark <jclarksummit@gmail.com>
2023-07-11 12:07:04 -0400

    Refactor base class with get_data method & static time limit methods

--- scrapers/base_scraper.py
+++ scrapers/base_scraper.py
@@ -7,16 +7,12 @@ from bs4 import BeautifulSoup
 from globalscrape import DEFAULT_HEADERS
 
 class Base_Scraper:
-    # def __init__(self, url_template):
-    #     self.url_template = url_template,
 
-    def scrape_data(self, stop_timestamp):
-        """Scrapes data from a provided webpage up to a given time limit"""
-        self.get_data(stop_timestamp)
-
-    def get_data(self):
+    def get_data(self, stop_timestamp):
         """Placeholder method overidden in subclass"""
-        pass
+        scraped_articles = self.get_news_articles_by_page(stop_timestamp=stop_timestamp)
+
+        return scraped_articles
 
     def get_news_articles_by_page(self):
         """Placeholder method overidden in subclass"""
@@ -24,7 +20,9 @@ class Base_Scraper:
 
     def get_soup(self, url):
         """Generates a response/gets soup from a given server using requests and
-        default headers"""
+        default headers
+        TODO: Add logic to handle server timeouts here.
+        """
 
         # bs4 setup
         response = requests.get(url, headers=DEFAULT_HEADERS)
@@ -37,7 +35,8 @@ class Base_Scraper:
     # Should they change in the future I only need to update them here rather
     # than in each individual scraper.
 
-    def reached_time_limit_loop(self, stop_timestamp=False, current_timestamp=False):
+    @staticmethod
+    def reached_time_limit_loop(stop_timestamp=False, current_timestamp=False):
         """Returns true if current_timestamp is earlier than limit, else returns
         false. Called when iterating through articles in subclasses.
         """
@@ -46,8 +45,8 @@ class Base_Scraper:
             return True
 
         return False
-
-    def reached_time_limit_recurse(self, stop_timestamp=False, current_timestamp=False):
+    @staticmethod
+    def reached_time_limit_recurse(stop_timestamp=False, current_timestamp=False):
         """Returns true if current timestamp hasn't reached limit, else returns
         false. Used in logic determining whether to recurse """
 
