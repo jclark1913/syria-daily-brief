a7b5fa47db1733ad32907bb6fafcd99d240cfb8f
Justin Clark <jclarksummit@gmail.com>
2023-07-14 00:49:44 -0400

    Add stub GET route for /api/collections

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 from models import db, connect_db, Collection, Entry
 
@@ -9,26 +9,46 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+app.config["SQLALCHEMY_ECHO"] = False
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 
 connect_db(app)
 
-## COLLECTIONS
+############# COLLECTIONS
+
 
 # GET ALL COLLECTIONS
+@app.get("/api/collections")
+def list_collections():
+    """API Route that returns all collections for a given user.
+
+    Returns: [
+        {id: 1, name: ..., ...},
+        {id: 2, name: ..., ...},
+        ...
+        ]
+    """
+
+    collections = [collection.serialize() for collection in Collection.query.all()]
+
+    return jsonify(collections)
+
 
 # GET SINGLE COLLECTION
 
 # UPDATE COLLECTION DETAILS
 
 # DELETE COLLECTION
-    # Migrate entries in collection to another collection
+# Migrate entries in collection to another collection
 
 # COPY FROM COLLECTION TO COLLECTION
 
 
-## ENTRIES
+############# ENTRIES
 
 # GET ALL ENTRIES FOR GIVEN COLLECTION
+
+# TRANSLATE GIVEN ENTRY
+
+# GET AI SUMMARY OF GIVEN ENTRY
