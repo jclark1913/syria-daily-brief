f2c5918cfbb6cacc46d5decae0c338c9b0e42ebf
Justin Clark <jclarksummit@gmail.com>
2023-07-15 15:15:53 -0400

    Update collection schema to handle validation

--- schemas.py
+++ schemas.py
@@ -6,8 +6,10 @@ ma = Marshmallow()
 
 
 class CollectionSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
     description = fields.String(required=False)
+    created_at = fields.Integer(dump_only=True)
 
     class Meta:
         fields = ("id", "name", "description", "created_at")
