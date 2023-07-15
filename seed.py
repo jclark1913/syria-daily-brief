b2076d9ebd8a1afec0353adaabc2fc543a22e0fe
Justin Clark <jclarksummit@gmail.com>
2023-07-15 11:52:01 -0400

    Update seed to fix autoincrement issue

--- seed.py
+++ seed.py
@@ -5,7 +5,9 @@ db.drop_all()
 db.create_all()
 
 # Create a test collection
-test_collection = Collection(id=1, name="Test_collection", description="TEST")
+test_collection = Collection(name="Test_collection", description="TEST")
+test_collection2 = Collection(name="Test_collection_2", description="TEST")
 
 db.session.add(test_collection)
+db.session.add(test_collection2)
 db.session.commit()
