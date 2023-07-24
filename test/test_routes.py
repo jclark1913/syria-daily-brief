56cc501fe38c95210ede6a3483056deaf032111a
Justin Clark <jclarksummit@gmail.com>
2023-07-24 17:39:34 -0400

    Finish collection routes and add scaffolding for entries routes

--- test/test_routes.py
+++ test/test_routes.py
@@ -1,9 +1,10 @@
 import os
 from unittest import TestCase, mock
-from sdb.app import app
 from sdb.models import db, Collection, Entry
 
 os.environ["DATABASE_URL"] = "postgresql:///sdb_test"
+
+from sdb.app import app
 
 db.drop_all()
 db.create_all()
@@ -15,6 +16,8 @@ class APICollectionsRoutesTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data"""
 
+        print(os.environ["DATABASE_URL"])
+
         Collection.query.delete()
         Entry.query.delete()
 
@@ -24,13 +27,15 @@ class APICollectionsRoutesTestCase(TestCase):
             name="Test Collection", description="Test Description"
         )
 
+        db.session.add(self.collection)
+
         self.entry = Entry(
             title="Test Entry",
             collection_id=self.collection.id,
             publication="Test Publication",
         )
 
-        db.session.add(self.collection)
+        db.session.add(self.entry)
         db.session.commit()
 
         self.collection_id = self.collection.id
@@ -84,6 +89,7 @@ class APICollectionsRoutesTestCase(TestCase):
 
         """Should return error message"""
         self.assertEqual(data["errors"]["name"][0], "Missing data for required field.")
+        self.assertEqual(data["errors"]["wrong_name"][0], "Unknown field.")
 
     def test_get_collection(self):
         """Does GET /api/collections/<collection_id> return a collection?"""
@@ -102,28 +108,109 @@ class APICollectionsRoutesTestCase(TestCase):
         """Does GET /api/collections/<collection_id> w/ return error if not found?"""
 
         response = self.client.get("/api/collections/1000")
-        data = response.json
 
         """Should return 404 status code"""
         self.assertEqual(response.status_code, 404)
 
-    # def test_update_collection(self):
-    #     """Does POST /api/collections/<collection_id> update a collection?"""
+    def test_update_collection(self):
+        """Does POST /api/collections/<collection_id> update a collection?"""
 
-    #     input = {"name": "Updated Collection", "description": "Updated Description"}
+        input = {"name": "Updated Collection", "description": "Updated Description"}
 
-    #     response = self.client.post(
-    #         f"/api/collections/{self.collection_id}", json=input
-    #     )
-    #     data = response.json
+        response = self.client.post(
+            f"/api/collections/{self.collection_id}", json=input
+        )
+        data = response.json
 
-    #     """Should return 200 status code"""
-    #     self.assertEqual(response.status_code, 200)
+        """Should return 200 status code"""
+        self.assertEqual(response.status_code, 200)
 
-    #     """Should return JSON of updated collection"""
-    #     self.assertEqual(data["updated"]["name"], "Updated Collection")
-    #     self.assertEqual(data["updated"]["description"], "Updated Description")
+        """Should return JSON of updated collection"""
+        self.assertEqual(data["Collection updated"]["name"], "Updated Collection")
+        self.assertEqual(
+            data["Collection updated"]["description"], "Updated Description"
+        )
 
-    #     """Should update collection in db"""
-    #     self.assertEqual(Collection.query.count(), 1)
-    #     self.assertEqual(Collection.query.get(1).name, "Updated Collection")
+        """Should update collection in db"""
+        self.assertEqual(Collection.query.count(), 1)
+        self.assertTrue(Collection.query.first().name == "Updated Collection")
+
+    def test_update_collection_invalid(self):
+        """Does POST /api/collections/<collection_id> w/ invalid data return error?"""
+
+        input = {"wrong_name": "Updated Collection"}
+
+        response = self.client.post(
+            f"/api/collections/{self.collection_id}", json=input
+        )
+
+        data = response.json
+
+        """Should return 400 status code"""
+        self.assertEqual(response.status_code, 400)
+
+        """Should return error message"""
+        self.assertEqual(data["errors"]["wrong_name"][0], "Unknown field.")
+
+    def test_delete_collection(self):
+        """Does DELETE /api/collections/<collection_id> delete a collection?"""
+
+        response = self.client.delete(f"/api/collections/{self.collection_id}")
+
+        """Should return 200 status code"""
+        self.assertEqual(response.status_code, 200)
+
+        """Should delete collection from db"""
+        self.assertEqual(Collection.query.count(), 0)
+
+    def test_delete_collection_invalid(self):
+        """Does DELETE /api/collections/<collection_id> w/ invalid id return error?"""
+
+        response = self.client.delete("/api/collections/1000")
+
+        """Should return 404 status code"""
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_entries_from_collection(self):
+        """Does GET /api/collections/<collection_id>/entries return a list of entries?"""
+
+        response = self.client.get(f"/api/collections/{self.collection_id}/entries")
+        data = response.json
+
+        """Should return 200 status code"""
+        self.assertEqual(response.status_code, 200)
+
+        """Should return a list of entries w/ correct data"""
+        print(data)
+        print(data["Test Collection"])
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data["Test Collection"][0]["title"], "Test Entry")
+
+
+class APIEntriesRoutesTestCase(TestCase):
+    """Tests for /api/entries routes"""
+
+    def setUp(self):
+        """Create test client, add sample data"""
+
+        Collection.query.delete()
+        Entry.query.delete()
+
+        self.client = app.test_client()
+
+        self.collection = Collection(
+            name="Test Collection", description="Test Description"
+        )
+
+        self.entry = Entry(
+            title="Test Entry",
+            collection_id=self.collection.id,
+            publication="Test Publication",
+        )
+
+        db.session.add(self.collection)
+        db.session.add(self.entry)
+        db.session.commit()
+
+        self.collection_id = self.collection.id
+        self.entry_id = self.entry.id
