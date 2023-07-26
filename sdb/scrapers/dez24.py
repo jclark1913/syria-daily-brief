399bb285fff921ef7397f0006bd8a1e0ee8cd8d9
Justin Clark <jclarksummit@gmail.com>
2023-07-26 18:25:38 -0400

    Refactor DEZ24 to include error handling:
    - Now returns ScrapeResult dataclass instead of article_list
    - Will catch error and return ScrapeResult even if scraping interrupted
    - No longer recurses to paginate

--- sdb/scrapers/dez24.py
+++ sdb/scrapers/dez24.py
@@ -1,8 +1,10 @@
-from sdb.scrapers.base_scraper import Base_Scraper
+from sdb.scrapers.base_scraper import BaseScraper
 import sdb.scrapers.utils as utils
+from sdb.scrapers.scrape_result import ScrapeResult
+from sdb.scrapers.scraping_error import ScrapingError
 
 
-class DEZ24(Base_Scraper):
+class DEZ24(BaseScraper):
     def __init__(self):
         self.url_template = "https://deirezzor24.net/category/%d8%a3%d8%ae%d8%a8%d8%a7%d8%b1/page/{page_num}/"
         self.publication = "Deir Ezzor 24"
@@ -10,67 +12,79 @@ class DEZ24(Base_Scraper):
     def get_news_articles_by_page(self, page_num=1, stop_timestamp=False):
         """Scrapes a single page of DEZ24 articles"""
 
-        # Generate correct url from template
-        url = self.url_template.format(page_num=page_num)
+        # Dataclass scrape result to be returned
+        scrape_result = ScrapeResult()
 
-        # bs4 setup
-        soup = self.get_soup(url=url)
-        articles = soup.find("div", class_="vce-loop-wrap").find_all("article")
+        while True:
+            # Generate correct url from template
+            url = self.url_template.format(page_num=page_num)
 
-        # List of articles to be returned
-        article_list = []
+            # bs4 setup
+            try:
+                soup = self.get_soup(url=url)
+            except ScrapingError as e:
+                print(f"Scraping error: {e}")
+                scrape_result.success = False
+                scrape_result.error_message = str(e)
+                return scrape_result
 
-        count = 1
+            articles = soup.find("div", class_="vce-loop-wrap").find_all("article")
 
-        # Gathers article info for each post on a single page
+            count = 1
 
-        for a in articles:
-            # Get article link
-            content = a.find("h2", class_="entry-title")
-            link = content.find("a").get("href")
+            # Gathers article info for each post on a single page
+            for a in articles:
+                # Get article link
+                content = a.find("h2", class_="entry-title")
+                link = content.find("a").get("href")
 
-            # Destructure last_updated and article_text
-            [last_updated, article_text] = list(
-                self.get_article_text_and_last_updated(link).values()
-            )
+                # Destructure last_updated and article_text
+                [last_updated, article_text] = list(
+                    self.get_article_text_and_last_updated(link).values()
+                )
 
-            # Get current timestamp for article
-            current_timestamp = utils.get_approx_timestamp_from_last_updated_AR(
-                last_updated
-            )
+                # Get current timestamp for article
+                try:
+                    current_timestamp = utils.get_approx_timestamp_from_last_updated_AR(
+                        last_updated
+                    )
+                except ScrapingError as e:
+                    print(f"Scraping error: {e}")
+                    scrape_result.success = False
+                    scrape_result.error_message = str(e)
+                    return scrape_result
 
-            # Breaks loop if timestamp reached
-            if self.reached_time_limit_loop(
+                # Breaks loop if timestamp reached
+                if self.reached_time_limit_loop(
+                    stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
+                ):
+                    return scrape_result
+
+                article = {
+                    "date_posted": current_timestamp,
+                    "title": content.text,
+                    "publication": self.publication,
+                    "link": link,
+                    "full_text": article_text,
+                }
+
+                # Send console message
+                self.entry_added_message(count=count, page_num=page_num)
+                count += 1
+
+                scrape_result.article_list.append(article)
+
+            # Checks if stop timestamp reached.
+            if not self.should_continue_pagination(
                 stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
             ):
-                break
+                return scrape_result
 
-            article = {
-                "date_posted": current_timestamp,
-                "title": content.text,
-                "publication": self.publication,
-                "link": link,
-                "full_text": article_text,
-            }
+            # Go to next page
+            page_num = page_num + 1
 
             # Send console message
-            self.entry_added_message(count=count, page_num=page_num)
-            count += 1
-
-            article_list.append(article)
-
-        # Recursively calls method for next page until stop_timestamp reached.
-        if self.reached_time_limit_recurse(
-            stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
-        ):
-            next_page_num = page_num + 1
-            # Send console message
-            self.next_page_message(page_num=next_page_num)
-            article_list += self.get_news_articles_by_page(
-                page_num=next_page_num, stop_timestamp=stop_timestamp
-            )
-
-        return article_list
+            self.next_page_message(count=count, page_num=page_num)
 
     def get_article_text_and_last_updated(self, article_link):
         """Gets the text from a single article as well as the description in
