3633d8a47da0c2a6e5366da267c94e0e2a119496
Justin Clark <jclarksummit@gmail.com>
2023-07-26 18:25:55 -0400

    Remove ScrapingError from utils

--- sdb/scrapers/utils.py
+++ sdb/scrapers/utils.py
@@ -143,11 +143,3 @@ def remove_RLM_char_from_str(str):
     """
 
     return str.replace("\u200f", "")
-
-
-class ScrapingError(Exception):
-    """Custom exception class for errors during web scraping."""
-
-    def __init__(self, message, url):
-        super().__init__(message)
-        self.url = url
