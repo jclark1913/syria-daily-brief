ff2727761a31588dd818a0c5865be5c795d7239a
Justin Clark <jclarksummit@gmail.com>
2023-07-26 19:02:27 -0400

    Update to handle errors while scraping

--- sdb/scrapers/syriadirect.py
+++ sdb/scrapers/syriadirect.py
@@ -1,10 +1,13 @@
+from sdb.scrapers.base_scraper import BaseScraper
+from sdb.scrapers.scrape_result import ScrapeResult
+from sdb.scrapers.scraping_error import ScrapingError
+
 import datetime
 
-from sdb.scrapers.base_scraper import Base_Scraper
 # import syriadailybrief.scrapers.utils as utils
 
 
-class SyriaDirect(Base_Scraper):
+class SyriaDirect(BaseScraper):
     def __init__(self):
         self.url_template = "https://syriadirect.org/%D8%A2%D8%AE%D8%B1-%D8%A7%D9%84%D8%AA%D9%82%D8%A7%D8%B1%D9%8A%D8%B1/page/{page_num}/?lang=ar"
         self.publication = "Syria Direct"
@@ -12,59 +15,73 @@ class SyriaDirect(Base_Scraper):
     def get_news_articles_by_page(self, page_num=1, stop_timestamp=False):
         """Scrapes a single page of Syria Direct articles until time limit reached"""
 
-        # Generate correct url from template
-        url = self.url_template.format(page_num=page_num)
+        # Dataclass scrape result to be returned
+        scrape_result = ScrapeResult()
+        url = self.url_template
 
-        # bs4 setup
-        soup = self.get_soup(url=url)
-        articles = soup.find("div", class_="fusion-posts-container").find_all("article")
+        while True:
+            url = url.format(page_num=page_num)
 
-        # List of articles to be returned
-        article_list = []
+            try:
+                # bs4 setup
+                soup = self.get_soup(url=url)
+            except ScrapingError as e:
+                print(f"Scraping error: {e}")
+                scrape_result.success = False
+                scrape_result.error_message = str(e)
+                return scrape_result
 
-        count = 1
-
-        # Gathers article info for each post on single page
-        for a in articles:
-            date_posted = a.find("span", class_="updated").text
-            current_timestamp = self.get_timestamp(date_posted)
-
-            # Breaks loop if timestamp reached
-            if self.reached_time_limit_loop(
-                stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
-            ):
-                break
-
-            title = a.find("h2").find("a").text
-            article = {
-                "date_posted": current_timestamp,
-                "title": title,
-                "publication": self.publication,
-                "link": a.find("h2").find("a").get("href"),
-            }
-
-            # Send console message
-            self.entry_added_message(count=count, page_num=page_num)
-            count += 1
-
-            # Add dict attribute for article text then append to article_list
-            article["full_text"] = self.get_article_text(article["link"])
-            article_list.append(article)
-
-        # Recursively calls method for next page until stop_timestamp reached.
-        if self.reached_time_limit_recurse(
-            stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
-        ):
-            next_page_num = page_num + 1
-
-            # Send console message
-            self.next_page_message(count=count, page_num=next_page_num)
-
-            article_list += self.get_news_articles_by_page(
-                page_num=next_page_num, stop_timestamp=stop_timestamp
+            articles = soup.find("div", class_="fusion-posts-container").find_all(
+                "article"
             )
 
-        return article_list
+            count = 1
+
+            # Gathers article info for each post on single page
+            for a in articles:
+                date_posted = a.find("span", class_="updated").text
+                current_timestamp = self.get_timestamp(date_posted)
+
+                # Returns result if timestamp reached
+                if self.reached_time_limit_loop(
+                    stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
+                ):
+                    return scrape_result
+
+                title = a.find("h2").find("a").text
+                article = {
+                    "date_posted": current_timestamp,
+                    "title": title,
+                    "publication": self.publication,
+                    "link": a.find("h2").find("a").get("href"),
+                }
+
+                # Send console message
+                self.entry_added_message(count=count, page_num=page_num)
+                count += 1
+
+                # Add dict attribute for article text then append to article_list
+                try:
+                    article["full_text"] = self.get_article_text(article["link"])
+                except ScrapingError as e:
+                    print(f"Scraping error: {e}")
+                    scrape_result.success = False
+                    scrape_result.error_message = str(e)
+                    return scrape_result
+
+                scrape_result.article_list.append(article)
+
+            # Checks if stop timestamp reached.
+            if not self.should_continue_pagination(
+                stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
+            ):
+                return scrape_result
+
+            # Go to next page
+            page_num = page_num + 1
+
+            # Send console message
+            self.next_page_message(count=count, page_num=page_num)
 
     def get_article_text(self, article_link):
         """Concatenates all paragraph elements in article into a single string,
