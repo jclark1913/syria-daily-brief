6bb2873cd5ebaf3952a6c60beddc5c22e1f0cf37
Justin Clark <jclarksummit@gmail.com>
2023-07-26 17:10:24 -0400

    Error handling refactor:
    - Removed recursive structure w/ simple while loop to handle pagination
    - Now returns article_list when error occurs
    - Added try/except blocks to all url requests to catch ScrapingErrors

--- sdb/scrapers/sana.py
+++ sdb/scrapers/sana.py
@@ -1,4 +1,5 @@
 from sdb.scrapers.base_scraper import Base_Scraper
+from sdb.scrapers.utils import ScrapingError
 
 # import syriadailybrief.scrapers.utils as utils
 
@@ -14,61 +15,70 @@ class SANA(Base_Scraper):
     def get_news_articles_by_page(self, page_num=1, stop_timestamp=False):
         """Scrapes a single page of SANA articles until time limit reached"""
 
-        # Generate correct url from template
-        url = self.url_template.format(page_num=page_num)
-
-        # bs4 setup
-        soup = self.get_soup(url=url)
-        articles = soup.find_all("article", class_="item-list")
-
         # List of articles to be returned
         article_list = []
 
-        count = 1
+        while True:
+            # Generate correct url from template
+            url = self.url_template.format(page_num=page_num)
 
-        # Gathers article info for each post on single page
-        for a in articles:
-            # identifies date posted and generates Unix timestamp
-            date_posted = a.find("span", class_="tie-date").text
-            current_timestamp = self.get_timestamp_SANA(date_posted)
+            # This try/except block is used to catch any errors that occur during
+            # scraping and return the article_list up to that point.
+            try:
+                # bs4 setup
+                soup = self.get_soup(url=url)
+            except ScrapingError as e:
+                print(f"Scraping error: {e}")
+                return article_list
 
-            # Breaks loop if timestamp reached
-            if self.reached_time_limit_loop(
+            articles = soup.find_all("article", class_="item-list")
+
+            count = 1
+
+            # Gathers article info for each post on single page
+            for a in articles:
+                # identifies date posted and generates Unix timestamp
+                date_posted = a.find("span", class_="tie-date").text
+                current_timestamp = self.get_timestamp_SANA(date_posted)
+
+                # Breaks loop if timestamp reached
+                if self.reached_time_limit_loop(
+                    stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
+                ):
+                    return article_list
+
+                # Gets title from card + creates dict of basic data
+                title = a.find("a", class_=None).text
+                article = {
+                    "date_posted": current_timestamp,
+                    "title": title,
+                    "publication": self.publication,
+                    "link": a.find("a", class_="more-link").get("href"),
+                }
+
+                # Send console message
+                self.entry_added_message(count=count, page_num=page_num)
+                count += 1
+
+                # Adds dict attribute for article text then appends to article_list.
+                try:
+                    article["full_text"] = self.get_article_text(article["link"])
+                except ScrapingError as e:
+                    print("Scraping error: {e}")
+                    return article_list
+                article_list.append(article)
+
+            # Recursively calls method for next page until stop_timestamp reached.
+            if not self.should_continue_pagination(
                 stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
             ):
-                break
+                return article_list
 
-            # Gets title from card + creates dict of basic data
-            title = a.find("a", class_=None).text
-            article = {
-                "date_posted": current_timestamp,
-                "title": title,
-                "publication": self.publication,
-                "link": a.find("a", class_="more-link").get("href"),
-            }
+            # Go to next page
+            page_num = page_num + 1
 
             # Send console message
-            self.entry_added_message(count=count, page_num=page_num)
-            count += 1
-
-            # Adds dict attribute for article text then appends to article_list
-            article["full_text"] = self.get_article_text(article["link"])
-            article_list.append(article)
-
-        # Recursively calls method for next page until stop_timestamp reached.
-        if self.reached_time_limit_recurse(
-            stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
-        ):
-            next_page_num = page_num + 1
-
-            # Send console message
-            self.next_page_message(count=count, page_num=next_page_num)
-
-            article_list += self.get_news_articles_by_page(
-                page_num=next_page_num, stop_timestamp=stop_timestamp
-            )
-
-        return article_list
+            self.next_page_message(count=count, page_num=page_num)
 
     def get_article_text(self, article_link):
         """Concatenates all paragraph elements in article into a single string and
