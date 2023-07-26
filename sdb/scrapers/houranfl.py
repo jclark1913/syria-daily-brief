8c92850e67d2fe3d221b3b1ffe795791a2c292df
Justin Clark <jclarksummit@gmail.com>
2023-07-26 18:56:21 -0400

    Refactor HFL scraper to include error handling during scraping

--- sdb/scrapers/houranfl.py
+++ sdb/scrapers/houranfl.py
@@ -1,11 +1,13 @@
-from sdb.scrapers.base_scraper import Base_Scraper
+from sdb.scrapers.base_scraper import BaseScraper
 import sdb.scrapers.utils as utils
+from sdb.scrapers.scraping_error import ScrapingError
+from sdb.scrapers.scrape_result import ScrapeResult
 
 import time
 import datetime
 
 
-class HouranFL(Base_Scraper):
+class HouranFL(BaseScraper):
     def __init__(self):
         self.url_template = "https://www.horanfree.com/page/{page_num}?cat=%2A"
         self.publication = "Houran Free League"
@@ -13,61 +15,78 @@ class HouranFL(Base_Scraper):
     def get_news_articles_by_page(self, page_num=1, stop_timestamp=False):
         """Scrapes a single page of HFL articles until time limit reached."""
 
-        # Generate correct url from template
-        url = self.url_template.format(page_num=page_num)
+        # Dataclass scrape result to be returned
+        scrape_result = ScrapeResult()
+        url = self.url_template
 
-        # bs4 setup
-        soup = self.get_soup(url=url)
-        articles = soup.find_all("li", class_="post-item")
+        while True:
+            # Generate correct url from template
+            url = url.format(page_num=page_num)
 
-        # List of articles to be returned
-        article_list = []
+            # bs4 setup
+            try:
+                soup = self.get_soup(url=url)
+            except ScrapingError as e:
+                print(f"Scraping error: {e}")
+                scrape_result.success = False
+                scrape_result.error_message = str(e)
+                return scrape_result
 
-        count = 1
+            articles = soup.find_all("li", class_="post-item")
 
-        # Gathers article info for each post on single page
-        for a in articles:
-            # identifies date posted and generates Unix timestamp
-            date_posted = a.find("span", class_="date meta-item tie-icon").text
-            current_timestamp = self.get_timestamp_from_arabic_latin_date_HFL(
-                date_posted
-            )
-            link = a.find("a", class_=None).get("href")
+            count = 1
 
-            # Breaks loop if timestamp reached
-            if self.reached_time_limit_loop(
+            # Gathers article info for each post on single page
+            for a in articles:
+                # identifies date posted and generates Unix timestamp
+                date_posted = a.find("span", class_="date meta-item tie-icon").text
+                current_timestamp = self.get_timestamp_from_arabic_latin_date_HFL(
+                    date_posted
+                )
+                link = a.find("a", class_=None).get("href")
+
+                # Breaks loop if timestamp reached
+                if self.reached_time_limit_loop(
+                    stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
+                ):
+                    return scrape_result
+
+                # Gets title and basic data and creates dict for article
+                article = {
+                    "date_posted": current_timestamp,
+                    "title": a.find("h2", class_="post-title").text,
+                    "link": link,
+                    "publication": self.publication,
+                }
+
+                # Adds dict attribute for article text then appends to article_list
+                try:
+                    article["full_text"] = self.get_article_text(article["link"])
+                except ScrapingError as e:
+                    print(f"Scraping error: {e}")
+                    scrape_result.success = False
+                    scrape_result.error_message = str(e)
+                    return scrape_result
+
+                # Send console message
+                self.entry_added_message(count=count, page_num=page_num)
+                count += 1
+                scrape_result.article_list.append(article)
+
+            # Checks if stop timestamp reached.
+            if not self.should_continue_pagination(
                 stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
             ):
-                break
+                return scrape_result
 
-            # Gets title and basic data and creates dict for article
-            article = {
-                "date_posted": current_timestamp,
-                "title": a.find("h2", class_="post-title").text,
-                "link": link,
-                "publication": self.publication,
-                "full_text": self.get_article_text(link),
-            }
+            # Go to next page
+            page_num = page_num + 1
 
             # Send console message
-            self.entry_added_message(count=count, page_num=page_num)
-            count += 1
-            article_list.append(article)
+            self.next_page_message(count=count, page_num=page_num)
 
-        # Recursively calls method for next page until stop_timestamp reached.
-        if self.reached_time_limit_recurse(
-            stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
-        ):
-            next_page_num = page_num + 1
-
-            # Send console message
-            self.next_page_message(count=count, page_num=next_page_num)
-
-            article_list += self.get_news_articles_by_page(
-                page_num=next_page_num, stop_timestamp=stop_timestamp
-            )
-
-        return article_list
+            if page_num > 2:
+                url = "dddddd"
 
     def get_article_text(self, article_link):
         """Gathers article text content from link to given article. Concatenates all
