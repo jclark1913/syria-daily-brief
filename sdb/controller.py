27ad99378ed4dca8614303438ad1445260cf73a7
Justin Clark <jclarksummit@gmail.com>
2023-07-27 10:59:34 -0400

    Update controller.py to handle ScrapeResult dataclass

--- sdb/controller.py
+++ sdb/controller.py
@@ -1,4 +1,5 @@
 import os
+
 print(os.getcwd())
 
 from sdb.scrapers import (
@@ -28,24 +29,35 @@ def run_selected_scrapers(selections, stop_timestamp, collection_id):
     """Controller function that takes an array of values corresponding to websites
     and a stop timestamp. The function will then call the correct scraper for
     each website and instruct it to grab data until the stop_timestamp is reached.
-
-    TODO: Current no error handling is in place at all.
     """
+
+    # List of dataclass objects to be returned
+    all_data = []
 
     # Empty list to be populated with entries
     entries = []
+
+    # Empty list for errors
+    errors = []
 
     # Iterate through selections and run each scraper individually
     for scraper in selections:
         scraper = scraper.value()
         print(f"Gathering data from {scraper.publication}")
-        articles = scraper.get_data(stop_timestamp=stop_timestamp)
-        entries += articles
+        data = scraper.get_data(stop_timestamp=stop_timestamp)
+        entries += data.article_list
 
-    # Add all our entries to db. TODO: Find a better home for this.
+        # We will save the dataclass object for each scraper in a list for reference
+        all_data.append(data)
+
+        # If scraper was unsuccessful, we should get errors as well
+        if not data.success:
+            errors.append(data.error_message)
+
+    # Add all our entries to db.
     add_entries_to_db(entries=entries, collection_id=collection_id)
 
-    return entries
+    return all_data, errors
 
 
 def add_entries_to_db(entries, collection_id):
@@ -58,7 +70,7 @@ def add_entries_to_db(entries, collection_id):
             link=e.get("link", ""),
             date_posted=e.get("date_posted", ""),
             full_text=e.get("full_text", ""),
-            publication=e.get("publication", "")
+            publication=e.get("publication", ""),
         )
         db.session.add(current_entry)
 
