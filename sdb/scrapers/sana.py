7ca2c8f6e057c1e2a25288b4e7affc1dd6a9a8dc
Justin Clark <jclarksummit@gmail.com>
2023-08-01 14:56:40 -0400

    Rewrite sana.py in line with new BaseScraper class, add SANA_Config

--- sdb/scrapers/sana.py
+++ sdb/scrapers/sana.py
@@ -1,92 +1,40 @@
-from sdb.scrapers.base_scraper import BaseScraper
+from sdb.scrapers.base_scraper import BaseScraper, ScraperConfig
 from sdb.scrapers.scraping_error import ScrapingError
 from sdb.scrapers.scrape_result import ScrapeResult
 
 import time
 import datetime
 
+SANA_config = ScraperConfig(
+    url_template="https://sana.sy/?cat=29582&paged={page_num}",
+    publication="SANA (Syrian Arab News Network)",
+    can_get_metadata_from_page=True,
+)
+
 
 class SANA(BaseScraper):
     def __init__(self):
-        self.url_template = "https://sana.sy/?cat=29582&paged={page_num}"
-        self.publication = "SANA (Syrian Arab News Network)"
+        self.config = SANA_config
 
-    def get_news_articles_by_page(self, page_num=1, stop_timestamp=False):
-        """Scrapes a single page of SANA articles until time limit reached"""
+    def find_all_articles(self, soup):
+        """Finds all articles on a single page and returns them as a list."""
 
-        # Dataclass scrape result to be returned
-        scrape_result = ScrapeResult()
+        return soup.find_all("article", class_="item-list")
 
-        url_template = self.url_template
+    def find_article_title(self, article):
+        """Returns the title of an article."""
 
-        while True:
+        return article.find("a", class_=None).text
 
-            # Generate correct url from template
-            url = url_template.format(page_num=page_num)
+    def find_article_link(self, article):
+        """Returns the link of an article."""
 
-            # This try/except block is used to catch any errors that occur during
-            # scraping and return the article_list up to that point.
-            try:
-                # bs4 setup
-                soup = self.get_soup(url=url)
-            except ScrapingError as e:
-                print(f"Scraping error: {e}")
-                scrape_result.success = False
-                scrape_result.error_message = str(e)
-                return scrape_result
+        return article.find("a", class_="more-link").get("href")
 
-            articles = soup.find_all("article", class_="item-list")
+    def find_article_date_posted(self, article):
+        """Returns the date posted of an article."""
 
-            count = 1
-
-            # Gathers article info for each post on single page
-            for a in articles:
-                # identifies date posted and generates Unix timestamp
-                date_posted = a.find("span", class_="tie-date").text
-                current_timestamp = self.get_timestamp_SANA(date_posted)
-
-                # Breaks loop if timestamp reached
-                if self.reached_time_limit_loop(
-                    stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
-                ):
-                    return scrape_result
-
-                # Gets title from card + creates dict of basic data
-                title = a.find("a", class_=None).text
-                article = {
-                    "date_posted": current_timestamp,
-                    "title": title,
-                    "publication": self.publication,
-                    "link": a.find("a", class_="more-link").get("href"),
-                }
-
-                # Send console message
-                self.entry_added_message(count=count, page_num=page_num)
-                count += 1
-
-                # Adds dict attribute for article text then appends to article_list.
-                try:
-                    article["full_text"] = self.get_article_text(article["link"])
-                except ScrapingError as e:
-                    print("Scraping error: {e}")
-                    scrape_result.success = False
-                    scrape_result.error_message = str(e)
-                    return scrape_result
-
-                scrape_result.article_list.append(article)
-
-
-            # Checks if stop timestamp reached.
-            if not self.should_continue_pagination(
-                stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
-            ):
-                return scrape_result
-
-            # Go to next page
-            page_num = page_num + 1
-
-            # Send console message
-            self.next_page_message(count=count, page_num=page_num)
+        return article.find("span", class_="tie-date").text
 
     def get_article_text(self, article_link):
         """Concatenates all paragraph elements in article into a single string and
@@ -99,7 +47,7 @@ class SANA(BaseScraper):
         paragraphs = soup.find("div", class_="entry").find_all("p", recursive=False)
         return "\n\n".join(paragraph.text for paragraph in paragraphs)
 
-    def get_timestamp_SANA(self, date):
+    def get_timestamp(self, date):
         """Takes date input and converts it to Unix timestamp
 
         NOTE: Assumes date is in YYYY-mm-dd format.
