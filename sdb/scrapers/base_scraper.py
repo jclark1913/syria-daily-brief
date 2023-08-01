b8adc4ec854e658e9bfce588633b8a38091d12be
Justin Clark <jclarksummit@gmail.com>
2023-08-01 14:56:12 -0400

    Refactor BaseScraper to avoid repetition:
    - Now uses namedtuple ScraperConfig to store url template, publication name, etc
    - get_news_articles_by_page is now a method stored on the base class that can be used for all subclasses.
    - Abstract methods added to point bs4 to the correct html elements during scraping.
    - Overall MUCH less needs to be rewritten whenever a new scraper is added.

--- sdb/scrapers/base_scraper.py
+++ sdb/scrapers/base_scraper.py
@@ -6,7 +6,19 @@ from bs4 import BeautifulSoup
 
 from sdb.scrapers.utils import DEFAULT_HEADERS
 
+from sdb.scrapers.scrape_result import ScrapeResult
 from sdb.scrapers.scraping_error import ScrapingError
+
+from collections import namedtuple
+
+ScraperConfig = namedtuple(
+    "ScraperConfig",
+    [
+        "url_template",
+        "publication",
+        "can_get_metadata_from_page",
+    ],
+)
 
 
 class BaseScraper(ABC):
@@ -14,6 +26,9 @@ class BaseScraper(ABC):
     used regularly by each scraper and allows for logic to be centralized and
     easily changed.
     """
+
+    def __init__(self):
+        self.config = None  # Overridden in subclasses
 
     def get_data(self, stop_timestamp):
         """Default method for initializing scraper. Can be called on any instanced
@@ -24,22 +39,13 @@ class BaseScraper(ABC):
 
         return scraped_articles
 
-    @abstractmethod
-    def get_news_articles_by_page(self):
-        """Placeholder method overidden in subclasses where articles are gathered
-        through pagination.
-        """
-        pass
-
     def get_soup(self, url):
         """Generates a response/gets soup from a given server using requests and
-        default headers
-        TODO: Add logic to handle server timeouts here.
+        default headers. If an error occurs, a ScrapingError is raised.
         """
 
         # bs4 setup: Attempts get request from server and prints error message
         # if any error occurs.
-
 
         # NOTE: This will raise a scraping error BUT it will retain the original
         # exception in the __cause__ atribute of the ScrapingError object. The
@@ -52,6 +58,126 @@ class BaseScraper(ABC):
         soup = BeautifulSoup(response.content, "html.parser")
 
         return soup
+
+    def get_news_articles_by_page(self, page_num=1, stop_timestamp=False):
+        """Placeholder method overidden in subclasses where articles are gathered
+        through pagination.
+        """
+        # Dataclass scrape result to be returned
+        scrape_result = ScrapeResult()
+        url_template = self.config.url_template
+
+        while True:
+            # Generate correct url from template
+            url = url_template.format(page_num=page_num)
+
+            # bs4 setup
+            try:
+                soup = self.get_soup(url=url)
+            except ScrapingError as e:
+                print(f"Scraping error: {e}")
+                scrape_result.success = False
+                scrape_result.error_message = str(e)
+                return scrape_result
+
+            # Gets all articles on page
+            articles = self.find_all_articles(soup)
+
+            count = 1
+
+            # Gathers article info for each post on single page
+            for a in articles:
+                # Gets article title
+                title = self.find_article_title(a)
+                link = self.find_article_link(a)
+
+                if self.config.can_get_metadata_from_page:
+                    date_posted = self.find_article_date_posted(a)
+                    current_timestamp = self.get_timestamp(date_posted)
+                    full_text = self.get_article_text(link)
+                else:
+                    [last_updated, full_text] = self.get_article_text_and_last_updated(
+                        link
+                    )
+                    print("LAST UPDATED: ", last_updated)
+                    current_timestamp = self.get_timestamp(last_updated)
+
+                # Breaks loop if timestamp reached
+                if self.reached_time_limit_loop(
+                    stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
+                ):
+                    print("LOOP LIMIT REACHED")
+                    return scrape_result
+
+                article = {
+                    "title": title,
+                    "date_posted": current_timestamp,
+                    "publication": self.config.publication,
+                    "link": link,
+                    "full_text": full_text,
+                }
+
+                # Send console message
+                self.entry_added_message(count=count, page_num=page_num)
+                print(current_timestamp)
+                count += 1
+
+                # Add article to scrape result
+                scrape_result.article_list.append(article)
+
+            # Checks if stop timestamp reached
+            if not self.should_continue_pagination(
+                stop_timestamp=stop_timestamp, current_timestamp=current_timestamp
+            ):
+                print(
+                    "PAGINATION LIMIT REACHED, STOP_TIMESTAMP= ",
+                    stop_timestamp,
+                    "CURRENT_TIMESTAMP= ",
+                    current_timestamp,
+                )
+                return scrape_result
+
+            # Go to next page
+            page_num = page_num + 1
+
+            # Send console message
+            self.next_page_message(count=count, page_num=page_num)
+
+    # NOTE: These abstract methods are all overridden in subclasses.
+
+    @abstractmethod
+    def find_all_articles(self, soup):
+        """Returns all articles on a page."""
+        pass
+
+    @abstractmethod
+    def find_article_title(self, article):
+        """Returns the title of an article."""
+        pass
+
+    @abstractmethod
+    def find_article_link(self, article):
+        """Returns the link of an article."""
+        pass
+
+    @abstractmethod
+    def find_article_date_posted(self, article):
+        """Returns the date an article was posted."""
+        pass
+
+    @abstractmethod
+    def get_timestamp(self, date_posted):
+        """Returns a timestamp from a date posted."""
+        pass
+
+    # NOTE: These methods are overridden in subclasses but are not abstract.
+    def get_article_text_and_last_updated(self, article):
+        """Returns text and last updated simultaneously"""
+        pass
+
+    def get_article_text(self, article_link):
+        """Returns the text of an article."""
+        pass
 
     def entry_added_message(self, count=1, page_num=1):
         """Prints a terminal message to the user when a new entry is added."""
