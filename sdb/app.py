7e474e4e7fb6d4e7e0c99a1a58dd38df73e9cd4e
Justin Clark <jclarksummit@gmail.com>
2023-09-09 11:39:16 -0400

    Link blueprints for /collections

--- sdb/app.py
+++ sdb/app.py
@@ -30,7 +30,11 @@ import sdb.translation as translation
 
 load_dotenv()
 
+from sdb.blueprints.collections.routes import collection
+
 app = Flask(__name__)
+app.register_blueprint(collection, url_prefix="/api/collections")
+
 ma = Marshmallow(app)
 
 # NOTE: Temporary fix while in development
@@ -53,108 +57,108 @@ from sdb.processes import cleanup_processes, ACTIVE_PROCESSES, STOP_EVENT
 ############# COLLECTIONS
 
 
-@app.get("/api/collections")
-def list_collections():
-    """API route that returns all collections for a given user.
+# @app.get("/api/collections")
+# def list_collections():
+#     """API route that returns all collections for a given user.
 
-    Returns: [
-        {id: 1, name: ..., ...},
-        {id: 2, name: ..., ...},
-        ...
-        ]
-    """
+#     Returns: [
+#         {id: 1, name: ..., ...},
+#         {id: 2, name: ..., ...},
+#         ...
+#         ]
+#     """
 
-    data = Collection.query.all()
-    collection_schema = CollectionSchema(many=True)
-    result = collection_schema.dump(data)
+#     data = Collection.query.all()
+#     collection_schema = CollectionSchema(many=True)
+#     result = collection_schema.dump(data)
 
-    return jsonify(result)
-
-
-@app.post("/api/collections")
-def create_new_collection():
-    """API route that adds a new collection to the db and returns it.
-
-    Returns: created:[
-        {id: 1, name:..., ...}
-    ]
-
-    """
-
-    # Get JSON and load schema
-    data = request.get_json()
-    collection_schema = CollectionSchema()
-
-    # Attempt to validate JSON data
-    result = collection_schema.load(data)
-
-    # Get data for new db instance
-    name = data["name"]
-    description = data.get("description", "")
-
-    # Update db
-    new_coll = Collection(name=name, description=description)
-    db.session.add(new_coll)
-    db.session.commit()
-
-    # Serialize and return new db instance
-    result = collection_schema.dump(new_coll)
-
-    return jsonify({"created": result})
+#     return jsonify(result)
 
 
-@app.get("/api/collections/<int:collection_id>")
-def get_collection(collection_id):
-    """Returns a single collection as JSON.
+# @app.post("/api/collections")
+# def create_new_collection():
+#     """API route that adds a new collection to the db and returns it.
 
-    Returns: {id: 1, name: ..., ...}
-    """
+#     Returns: created:[
+#         {id: 1, name:..., ...}
+#     ]
 
-    collection = Collection.query.get_or_404(collection_id)
-    collection_schema = CollectionSchema()
-    result = collection_schema.dump(collection)
+#     """
 
-    return jsonify(result)
+#     # Get JSON and load schema
+#     data = request.get_json()
+#     collection_schema = CollectionSchema()
 
+#     # Attempt to validate JSON data
+#     result = collection_schema.load(data)
 
-@app.post("/api/collections/<int:collection_id>")
-def update_collection(collection_id):
-    """Updates a given collection:
+#     # Get data for new db instance
+#     name = data["name"]
+#     description = data.get("description", "")
 
-    Returns: {'message': 'Collection updated'}
-    """
+#     # Update db
+#     new_coll = Collection(name=name, description=description)
+#     db.session.add(new_coll)
+#     db.session.commit()
 
-    data = request.get_json()
-    curr_coll = Collection.query.get_or_404(collection_id)
-    collection_schema = CollectionSchema()
+#     # Serialize and return new db instance
+#     result = collection_schema.dump(new_coll)
 
-    collection_schema.load(data, partial=True)
-
-    for field, value in data.items():
-        if hasattr(curr_coll, field):
-            setattr(curr_coll, field, value)
-
-    db.session.commit()
-
-    result = collection_schema.dump(curr_coll)
-
-    return jsonify({"Collection updated": result})
+#     return jsonify({"created": result})
 
 
-# TODO: Handle migration of entries when collection is deleted
-@app.delete("/api/collections/<int:collection_id>")
-def delete_collection(collection_id):
-    """Deletes a given collection
+# @app.get("/api/collections/<int:collection_id>")
+# def get_collection(collection_id):
+#     """Returns a single collection as JSON.
 
-    Returns: {'message': 'Collection deleted'}
-    """
+#     Returns: {id: 1, name: ..., ...}
+#     """
 
-    curr_coll = Collection.query.get_or_404(collection_id)
-    db.session.delete(curr_coll)
+#     collection = Collection.query.get_or_404(collection_id)
+#     collection_schema = CollectionSchema()
+#     result = collection_schema.dump(collection)
 
-    db.session.commit()
+#     return jsonify(result)
 
-    return jsonify({"message": "Deleted collection"})
+
+# @app.post("/api/collections/<int:collection_id>")
+# def update_collection(collection_id):
+#     """Updates a given collection:
+
+#     Returns: {'message': 'Collection updated'}
+#     """
+
+#     data = request.get_json()
+#     curr_coll = Collection.query.get_or_404(collection_id)
+#     collection_schema = CollectionSchema()
+
+#     collection_schema.load(data, partial=True)
+
+#     for field, value in data.items():
+#         if hasattr(curr_coll, field):
+#             setattr(curr_coll, field, value)
+
+#     db.session.commit()
+
+#     result = collection_schema.dump(curr_coll)
+
+#     return jsonify({"Collection updated": result})
+
+
+# # TODO: Handle migration of entries when collection is deleted
+# @app.delete("/api/collections/<int:collection_id>")
+# def delete_collection(collection_id):
+#     """Deletes a given collection
+
+#     Returns: {'message': 'Collection deleted'}
+#     """
+
+#     curr_coll = Collection.query.get_or_404(collection_id)
+#     db.session.delete(curr_coll)
+
+#     db.session.commit()
+
+#     return jsonify({"message": "Deleted collection"})
 
 
 # @app.get("/api/collections/<int:collection_id>/entries")
