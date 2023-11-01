c53c6b82a627abba7684e92b07e489af01ce0e61
Justin Clark <jclarksummit@gmail.com>
2023-11-01 16:29:31 -0400

    Add basic search functionality for entries

--- sdb/blueprints/entries/routes.py
+++ sdb/blueprints/entries/routes.py
@@ -56,3 +56,57 @@ def delete_single_entry(entry_id):
 
     return jsonify({"Deleted entry": entry_id})
 
+@entry.post("/search")
+def search_entries():
+    """Searches entries for given query
+
+    Returns: {entries: [{id: ..., title: ..., ...}, ...]}
+
+{
+    "date_range": {
+        "start_date": "YYYY-MM-DD",
+        "end_date": "YYYY-MM-DD"
+    },
+    "search_terms": [
+        {
+            "term": "Syria",
+            "columns": ["publication", "article_summary"]
+        },
+        {
+            "term": "Russia",
+            "columns": ["article_title"]
+        }
+        // ... add more search terms as needed
+    ]
+}
+
+    """
+
+    data = request.get_json()
+    collection_id = data.get("collection_id")
+    entries = Entry.query.filter_by(collection_id=collection_id)
+
+    search_terms = data.get("search_terms", [])
+    date_range = data.get("date_range", {})
+
+    start_date = date_range.get("start_date")
+    end_date = date_range.get("end_date")
+
+    if start_date:
+        entries = entries.filter(Entry.date >= start_date)
+    if end_date:
+        entries = entries.filter(Entry.date <= end_date)
+
+    for term_info in search_terms:
+        term = term_info.get("term")
+        columns = term_info.get("columns", [])
+        for col in columns:
+            if hasattr(Entry, col):
+                entries = entries.filter(getattr(Entry, col).ilike(f"%{term}%"))
+
+    entries = entries.all()
+    entry_schema = EntrySchema(many=True)
+    result = entry_schema.dump(entries)
+
+    return jsonify({"entries": result})
+
